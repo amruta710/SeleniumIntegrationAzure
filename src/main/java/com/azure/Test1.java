fc8bfe4cfb3293aee90a484fea8eecace5521f58
amruta.deshmukh <amruta.deshmukh@IMP-ITPL0916.impetus.co.in>
2024-08-09 10:29:30 +0530

    new commit

--- src/main/java/com/azure/Test1.java
+++ src/main/java/com/azure/Test1.java
@@ -8,4 +8,7 @@ public class Test1 {
  	@Test
 	public void azureTestCases() {
     	Assert.assertEquals(1,1);
-		System.out.println("hellow");}}
+		System.out.println("hellow");
+		System.out.println("hellow");
+		}
+ 	}
